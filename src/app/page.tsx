dab8fa700e111ccc3fd7d866f3e77b6d91ca4903
Muhammed Suhaib <suhaib.muhammed2002@gmail.com>
2025-08-04 17:25:46 +0000

    remove the fixed top navigation bar. also change the colors. i want it t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Header from "@/components/landing/Header";
 import Hero from "@/components/landing/Hero";
-import Footer from "@/components/landing/Footer";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-grow">
         <Hero />
       </main>
-      <Footer />
     </div>
   );
 }
